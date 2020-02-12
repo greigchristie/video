8b6161a13176aefab98620199dfdc74ed6de324e
Greig Christie <greig.christie@ed.ac.uk>
2020-02-12 10:40:29 +0000

    Improved html title. Added year after video name

--- resource.php
+++ resource.php
@@ -4,8 +4,8 @@ include('pre_header.php');
 include('db_movie.php');
 $resource_type = $_GET['resource'];
 $resource_id = $_GET['resource_id'];
-echo "<title>resource.php - $resource_type; $resource_id</title>";
-include('header.php');
+// echo "<title>resource.php - $resource_type; $resource_id</title>";
+// include('header.php');
 $table_variable = "movie_$resource_type";
 $id_variable = $table_variable."_id";
 
@@ -22,12 +22,14 @@ $result = getResourceDetails ($resource_type,$resource_id);
 		$name_variable .= "_full_name";
 	}
 		$resource_name = $row[$name_variable];
+		echo "<title>$resource_name Videos</title>";
+		include('header.php');
 		echo "<h2>$resource_name</h2>";
 		// echo "<p>getting resource_name</p>";
 		// print_r($row);
 	}
 
-$movie_xd = getMovieForResource($mri_id,$resource_id); 
+$movie_xd = getMovieForResource($mri_id,$resource_id);
 	while ($mov_res = $movie_xd->fetch_assoc()){
 		// echo "<p>getting movie_id for resource</p>";
 		$movie_id = $mov_res['movie_id'];
@@ -35,7 +37,8 @@ $movie_xd = getMovieForResource($mri_id,$resource_id);
 		$movie_all = getMovieDetails($movie_id);
 		while ($vie_res = $movie_all->fetch_assoc()) {
 		$movie_title = $vie_res['movie_title'];
-		echo "<p><a href='movie.php?movie=$movie_id'>$movie_title</a></p>";
+		$movie_year = $vie_res['movie_year'];
+		echo "<p><a href='movie.php?movie=$movie_id'>$movie_title ($movie_year)</a></p>";
 	}
 	}
 
