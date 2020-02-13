0b8f826b80fcd868b39f3476dd34dfc3ff920c81
greigchristie <greig.christie@ed.ac.uk>
2020-02-13 21:47:00 +0000

    some html styling for all_movies

--- all_movies.php
+++ all_movies.php
@@ -3,9 +3,9 @@ include('pre_header.php');
 echo "<title>all_movies.php</title>";
 include('header.php');
 include('db_movie.php');
-
+echo "<h2>All Movies & Series</h2>";
 $result = getAllMovies();
-
+echo "<p>";
 while ($row = $result->fetch_assoc()) {
 	$movie_title= $row['movie_title'];
 	$movie_id= $row['movie_id'];
@@ -13,5 +13,6 @@ while ($row = $result->fetch_assoc()) {
 	echo "<p><a href='movie.php?movie=$movie_id'>$movie_title</a></p>";
 	echo "</div>";
 }
+echo "</p>";
 include('footer.php');
 ?>
