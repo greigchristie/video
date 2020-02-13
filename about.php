8396874933a700e5431f8559b62a38508aee260e
Greig Christie <greig.christie@ed.ac.uk>
2020-02-13 13:58:24 +0000

    Basic information for about and posters pages. Making sure that all the header links work correctly

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+  include('pre_header.php');
+  echo "<title>About This Site</title>";
+  include('header.php');
+
+  ?>
+    <h2>Movies, TV Series etc.</h2>
+    <p>This is a reimagining of my old movies site.<br>
+    Most significant is a complete database overhaul.<br>
+    It also uses OMDB as it's source for 'golden copy' information.<br>
+    Importantly it's been designed so that each film/series has one entry.<br>
+    If I own multiple copies then the code is written so that all item information can be seen on one entry.<br>
+    It's also designed to understand that TV shows can have multiple seasons and individual episodes.
+    </p>
+<?php
+  include('footer.php');
+ ?>
---
+++ posters.php
@@ -0,0 +1,11 @@
+<?php
+  include('pre_header.php');
+  echo "<title>About This Site</title>";
+  include('header.php');
+
+  ?>
+<h2>POSTERS</h2>
+<p>There are no posters as yet. Maybe if I pay the extra to get access to the OMDB poster API</p>
+<?php
+  include('footer.php');
+?>
