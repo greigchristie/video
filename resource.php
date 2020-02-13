87c4f020edc190494c9f983bbcd250a02b1d49bc
Greig Christie <greig.christie@ed.ac.uk>
2020-02-13 14:08:05 +0000

    Use new query to get movie details. Reduces number of db calls needed.

--- resource.php
+++ resource.php
@@ -9,12 +9,21 @@ $resource_id = $_GET['resource_id'];
 $table_variable = "movie_$resource_type";
 $id_variable = $table_variable."_id";
 
+//get id for named resource type
 $mri = getResourceIdByName($resource_type);
 	while ($mri_res = $mri->fetch_assoc()) {
 		$mri_id = $mri_res['movie_resources_id'];
 		// echo "<p>MRI_ID: $mri_id</p>";
 	}
 
+// $result = getAllMoviesForResourceType($mri_id, $resource_id);
+// 	while ($row = $result->fetch_assoc()) {
+// 		$movie_id = $row['movie_id'];
+// 		$movie_title = $row['movie_title'];
+// 		$movie_year = $row['movie_year'];
+// 	}
+
+// get the name of the chosen resource from the specific resource table
 $result = getResourceDetails ($resource_type,$resource_id);
 	while ($row = $result->fetch_assoc()) {
 		$name_variable = $table_variable;
@@ -29,17 +38,16 @@ $result = getResourceDetails ($resource_type,$resource_id);
 		// print_r($row);
 	}
 
-$movie_xd = getMovieForResource($mri_id,$resource_id);
+// get all movies for the named resource
+$movie_xd = getAllMoviesForResourceType($mri_id,$resource_id);
 	while ($mov_res = $movie_xd->fetch_assoc()){
 		// echo "<p>getting movie_id for resource</p>";
 		$movie_id = $mov_res['movie_id'];
 
-		$movie_all = getMovieDetails($movie_id);
-		while ($vie_res = $movie_all->fetch_assoc()) {
-		$movie_title = $vie_res['movie_title'];
-		$movie_year = $vie_res['movie_year'];
+		$movie_title = $mov_res['movie_title'];
+		$movie_year = $mov_res['movie_year'];
 		echo "<p><a href='movie.php?movie=$movie_id'>$movie_title ($movie_year)</a></p>";
-	}
+
 	}
 
 
