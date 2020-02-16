05d6b980d0b262a35653074a70afff985ba4b9fb
greigchristie <greig.christie@ed.ac.uk>
2020-02-16 18:04:34 +0000

    Changes to improve header navigation and look

--- header.php
+++ header.php
@@ -1,24 +1,52 @@
 
-
-	<style type="text/css" media="all">
-		@import "css/lavish-bootstrap.css";
-	</style>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 
 <body>
  <div class="container">
-	<div class="row hidden-print">
-		<div class="col-xs-12">
-		<ul class="list-inline">
-			<li><a href="index.php">SEARCH</a></li>
-			<li><a href="all_movies.php">MOVIES</a></li>
-			<li><a href="recent_movies.php">RECENT</a></li>
-			<li><a href="posters.php">POSTERS</a></li>
-			<li><a href="about.php">ABOUT</a></li>
-		</ul>
-	</div>
-	</div>
+	 <div class="row hidden-print">
+
+ 		<!-- <ul class="nav navbar-nav"> -->
+ 		<nav class="navbar navbar-default">
+ 			<div class="container-fluid">
+ 				<div class="navbar-header">
+       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse-1" aria-expanded="false">
+         <span class="sr-only">Toggle navigation</span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+       </button>
+
+       <a class="navbar-brand" href="index.php"><img src="images/e7-2-40.png"></a>
+
+     </div>
+     <div class="collapse navbar-collapse" id="collapse-1">
+ 		<ul class="nav navbar-nav">
+ 		<li><a href="index.php">SEARCH</a></li>
+ 		<li class="dropdown">
+           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">BROWSE <span class="caret"></span></a>
+           <ul class="dropdown-menu">
+           	<li><a href="all_movies.php">All Videos</a></li>
+ 			<li><a href="movies.php">All Movies</a></li>
+ 			<li><a href="series.php">All Series</a></li>
+           </ul>
+         </li>
+         <li class="dropdown">
+           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">RECENT <span class="caret"></span></a>
+           <ul class="dropdown-menu">
+ 			<li><a href="recent_movies.php">RECENT Movies</a></li>
+ 			<li><a href="recent_series.php">RECENT Series</a></li>
+           </ul>
+         </li>
+
+ 			<li><a href="posters.php">POSTERS</a></li>
+ 			<li><a href="about.php">ABOUT</a></li>
+
+ 		</ul>
+ 	</div>
+ 			</div>
+ 	</nav>
+
+ 	</div>
 <div class="row hidden-print">
 <div class="col-xs-12 col-md-8 col-lg-8"><h1>EVERYTHING <small>DVDs, Blu-Rays, VHS, &amp; Digital</small></h1></div>
 <div class="col-xs-12 col-md-4 col-lg-4 well well-sm text-right">
--- pre_header.php
+++ pre_header.php
@@ -3,3 +3,9 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
+  	<style type="text/css" media="all">
+  		@import "css/lavish-bootstrap.css";
+  	</style>
+  	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
